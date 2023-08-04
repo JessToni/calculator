7b99870270fc211327bb12904f0ca9cc999e2c3b
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-04 16:26:29 +0800

    Display currentValue to currentScreen

--- calculator.js
+++ calculator.js
@@ -16,9 +16,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
     numbers.forEach((number) => number.addEventListener("click", function(e) {
         handleNumber(e.target.textContent);
+        currentScreen.textContent = currentValue;
     })) 
 })
 
 function handleNumber(num) {
-    console.log(num);
+    currentValue += num;
 }
