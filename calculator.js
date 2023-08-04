2e63fab2b2d5fb1dab6b044e93ca468aa61c72de
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-04 19:09:23 +0800

    Extend max displayed result length to 9

--- calculator.js
+++ calculator.js
@@ -37,10 +37,10 @@ document.addEventListener("DOMContentLoaded", function () {
         if (currentValue != '' && previousValue != '') {
             calculate();
             previousScreen.textContent = '';
-            if (previousValue.length <= 5) {
+            if (previousValue.length <= 9) {
             currentScreen.textContent = previousValue;
             } else {
-                currentScreen.textContent = previousValue.slice(0,5) + "..."
+                currentScreen.textContent = previousValue.slice(0,8) + "..."
             }
         }
     })
