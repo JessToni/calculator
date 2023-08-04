eeb7e80da7ea25eb4f8077f20336bb82f9882a29
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-04 16:17:46 +0800

    Access all of the number buttons using forEach

--- calculator.js
+++ calculator.js
@@ -13,4 +13,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let previousScreen = document.querySelector(".previous");
     let currentScreen = document.querySelector(".current");
+
+    numbers.forEach((number) => number.addEventListener("click", function(e) {
+        handleNumber(e.target.textContent);
+    })) 
 })
+
+function handleNumber(num) {
+    console.log(num);
+}
