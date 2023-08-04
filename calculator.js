82749476f5932d0da807e244d646b5c90fd155d8
Jess Antoni C. Mosquera <jcmsquera@up.edu.ph>
2023-08-04 16:05:45 +0800

    Store all HTML compnents into JS

--- calculator.js
+++ calculator.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     //Store all HTML components into JS.
-    
+    let clear = document.querySelector("#clear-button");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
 })
